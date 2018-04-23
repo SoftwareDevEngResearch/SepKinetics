764018ede9f1cfd1462c43ba22fb3e405174600c
David J Bettinardi <38048705+DavidBettinardi@users.noreply.github.com>
2018-04-23 15:58:46 -0700

    Added tests

--- SepKinetics/importing.py
+++ SepKinetics/importing.py
@@ -4,22 +4,35 @@ import matplotlib.pyplot as plt
 import plotly.offline as py
 import plotly.graph_objs as go
 
+'''# TODO:
+-use argv in __main__ or hardcode .csv for testing?
+-fix 2D plotting re: import
+-fix 3D y-axis
+-add axis labels
+-add monocolor histogram for 2D plot
+-add plotting series of histograms (see PythonProject folder in chrome)
+'''
+
 """ 04-22-18 - David j Bettinardi - SepKinetics software
-This script (so far) imports .csv kinetic data from the Olis CLARiTY
-Spectrophotometer and outputs a 3-dimentional visualizerself.
-Three datastructure approaches were attempted, current approach
-is through -m pandas"""
+The script imports .csv kinetic data from the Olis CLARiTY
+spectrophotometer and outputs 2D and 3D visualizers.
+Three datastructure approaches were attempted; current approach
+uses dataframes in -m pandas"""
 
 
-'''call in terminal with $ python importing.py filename.csv'''
+'''call in terminal using $ python importing.py filename.csv'''
 
+filename = 'kintest.csv'        # not overwritten in __main__
 
-# pandas approach
+# pandas approach [third try]
 def main():
-    filename = sys.argv[1]
-    df = pd.read_csv(filename, index_col=0, header = 0)
-    #print df.head(n=1)
-    #print df
+    #filename = sys.argv[1]      # command prompt argument 1
+
+    df = pd.read_csv(filename, index_col=0, header = 0) # set column 1 as index; first row as header
+    #t = df.head(n=1)
+    #print df.iloc[0,0]
+    #timepoint = [float(i) for i in list(df)]
+    # df
 
     #df.iloc[0,0] = 'nm'      # sets first value to wavelength (nm)
     #wavelength = df.ix[:]['0']      # designates first column as wavelength
@@ -31,13 +44,17 @@ def main():
 df = main()
 
 
-# Make a 2D plot of first spectrum
-def two_dplot():
-    x = df.iloc[1:,0]
-    y = df.iloc[1:,1:]
-    print x,y
-    plt.plot(x,y)
-    plt.show()
+
+# Make an overlapping 2D plot of all spectra
+def two_dplot():        # BROKEN FUNCTION
+    #filename = sys.argv[1]
+    #df = pd.read_csv(filename, header = 0)
+    wavelength = df.iloc[1:,0]
+    absorb = df.iloc[1:,1:]
+    #print wavelength, abs
+
+    plt.plot(wavelength,absorb)
+    return plt.show()
 #two_dplot()
 #print df.iloc[0,1:]
 #z = df.iloc[0,1:]
@@ -45,17 +62,19 @@ def two_dplot():
 #print z
 
 
-# Make a 3D plot    # Must goto: Edit in Chart Studio > delete z[0]
+# Make a 3D plot
 def three_dplot():
     data = [go.Surface(z=df.as_matrix(), colorscale='Viridis')]
     #data = [go.Surface(x=x, y=y, z=z)]
     #print data
     fig = go.Figure(data=data)
-    py.plot(fig)
+
+    #return py.plot(fig)
 three_dplot()
 
 
-# numpy approach
+
+# numpy approach [second try]
 '''
 filename = sys.argv[1]      # read filename.csv from command line
 data = np.loadtxt(filename, delimiter=',', skiprows = 1, unpack=True)     # unpack = rows -> columns
@@ -70,7 +89,7 @@ plt.plot(wavelength, data[1, 1:])
 '''
 
 
-# dictionary approach
+# dictionary approac [first try]
 '''
 filename = sys.argv[1]
 f = csv.DictReader(open(filename))
@@ -88,3 +107,7 @@ for row in f:   # defines the dictionary with each spectrum
 
 print timepoint     # checks output
 '''
+
+if __name__ == '__main__':
+
+    filename = sys.argv[1]
