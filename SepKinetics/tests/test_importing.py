d04d5e63ef9829505de61aae5c979ede5144374f
David J Bettinardi <38048705+DavidBettinardi@users.noreply.github.com>
2018-04-26 12:38:05 -0700

    os.path.join

--- SepKinetics/tests/test_importing.py
+++ SepKinetics/tests/test_importing.py
@@ -1,9 +1,11 @@
 """Test scipts for importing.py"""
 #from sepkinetics.importing import main, two_dplot, three_dplot
 from .. import importing as im
+import os
 
 def test_main():
-    df = im.main('./kintest.csv')
+    fn = os.path.join(os.path.dirname(__file__), 'kintest.csv')
+    df = im.main(fn)
     assert 0 < df.iloc[0,0] < 1
 
     timepoints = [float(i) for i in list(df)]
