083ea353db5253b0c11114bc84899850527ae084
David J Bettinardi <38048705+DavidBettinardi@users.noreply.github.com>
2018-05-02 15:21:38 -0700

    Added peakfind function, added peakshow function, fixed double importing

--- SepKinetics/importing.py
+++ SepKinetics/importing.py
@@ -3,19 +3,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.offline as py
 import plotly.graph_objs as go
+import peakutils
 
 '''# TODO:
+-add autobaseline correction
+-check Martin's disseration for deconvolution
 -add more tests
--add gitignore file
 -add google style comments to code
--use argv in __main__ or hardcode .csv for testing?
--import .xlsx instead of .csv (comes from clarity as .xlsx)
--save 3D plots as HTML
--fix double importing
+-import .xlsx instead of .csv (comes from clarity as .xlsx)?
+-save 3D plots as HTML?
+-fix double importing -- FIXED
+-fix peakfinding x-axis
+-add 2D peak labels
 -fix 3D y-axis
--add axis labels
--add monocolor histogram for 2D plot
--add peak finding function
+-add monocolor histogram for 2D plot  -- ADDED
+-add peak finding function -- ADDED
 -add plotting series of histograms (see PythonProject folder in chrome)
 '''
 
@@ -25,41 +27,108 @@ spectrophotometer and outputs 2D and 3D visualizers.
 Three datastructure approaches were attempted; current approach
 uses dataframes in -m pandas"""
 
-
 '''call in terminal using $ python importing.py filename.csv'''
-
-
-filename = 'scan10.csv'     # hardcoded for testing; overidden in __main__
 
 
 # pandas approach [third try]
 def main(filename):
     """Creates dataframe with top row and first column as headers"""
-    df = pd.read_csv(filename, index_col=0, header = 0) # set column 1 as index; first row as header
+    df = pd.read_csv(filename, header = 0) # first row as header
     return df
 
 
 def two_dplot():
     """Makes an overlapping 2D plot of wavelength vs. kinetics @ all timepoints"""
-    #filename = sys.argv[1]          # reads in again; MUST FIX
-    df = pd.read_csv(filename, header = 0)      # reads in again; MUST FIX
-    wavelength = df.iloc[1:,0]      # set firts column as wavelength
-    absorb = df.iloc[1:,1:]     # set column as absorbances
-    plt.plot(wavelength,absorb)
+    try:        # duck typing to allow test_function exceptions
+        filename = sys.argv[1]
+    except:
+        pass
+    df = main(filename)
+    df.set_index('0', inplace = True)   #reset index as time-series
+    df.plot(legend=False, colormap = 'coolwarm')
     return plt.show()
-#two_dplot()
+two_dplot()
 
 
 def three_dplot():
     """Makes a 3D surface plot"""
-    #filename = sys.argv[1]          # reads in again; MUST FIX
-    df = pd.read_csv(filename, index_col=0, header = 0)     # reads in again; MUST FIX
-    data = [go.Surface(z=df.as_matrix(), colorscale='Viridis')]     # transform to matrix for surface plot
-    fig = go.Figure(data=data)      # make figure
-    return py.plot(fig)             # plot figure
+    try:        # duck typing to allow test_function exceptions
+        filename = sys.argv[1]
+    except:
+        pass
+    df = main(filename)
+    df2 = df.set_index('0')
+
+    y = df.iloc[:,0].tolist()   # attempt to overwrite y-axis
+
+    '''Working plotter, wrong y-axis!'''
+    data = [go.Surface(z=df2.as_matrix(), y=y, colorscale='Viridis')]     # transform to matrix for surface plot
+
+    layout = go.Layout(
+                    scene = dict(
+                    xaxis = dict(
+                        title='Time (s)'),
+                    yaxis = dict(
+                        title='Wavelength (nm)'),
+                    zaxis = dict(
+                        title='Abs'),),
+                    width=700,
+                  )
+    fig = go.Figure(data=data, layout=layout)
+    return py.plot(fig)
 #three_dplot()
 
 
-if __name__ == '__main__':
+def peakfind():
+    """Finds major/minor peaks"""
+    try:        # duck typing to allow test_function exceptions
+        filename = sys.argv[1]
+    except:
+        pass
+    df = main(filename)
+    df.set_index('0', inplace = True)   # reset index as time-series
+    midpoint = (len(df.iloc[0,:]))/2    # find the mid-timepoint
+    midpoint_spectrum = df.iloc[:, midpoint]    # find the midpoint spectrum
+    # deploy peak findning on this spectrum
+    indices = peakutils.indexes(midpoint_spectrum, thres=0.02/max(midpoint_spectrum))
+    return indices, midpoint_spectrum
 
-    filename = sys.argv[1]
+
+def peakshow():
+    indices, midpoint_spectrum = peakfind() # obtain peak data
+
+    # trace the midpoint spectrum
+    spectrum = go.Scatter(
+        x=[j for j in range(len(midpoint_spectrum))],
+        y=midpoint_spectrum,
+        mode='lines',
+        name='Abs spectrum of the midpoint-time in kinetic series'
+    )
+
+    # mark the peaks
+    peaks = go.Scatter(
+        x=indices,
+        y=[midpoint_spectrum.iloc[j] for j in indices],
+        mode='markers',
+        marker=dict(
+            size=8,
+            color='rgb(255,0,0)',
+            symbol='cross'
+        ),
+        name='Detected Peaks'
+    )
+
+    data = [spectrum, peaks]
+    py.plot(data)
+    return plt.show()
+#peakshow()
+
+#def peakkinetics():
+
+
+
+if __name__ == '__main__':
+    try:
+        filename = sys.argv[1]
+    except:
+        raise ValueError('Specify the .csv you wish to import')
--- SepKinetics/tests/test_importing.py
+++ SepKinetics/tests/test_importing.py
@@ -1,16 +1,17 @@
 """Test scipts for importing.py"""
-#from sepkinetics.importing import main, two_dplot, three_dplot
-from .. import importing as im
+
+from .. import importing
+import numpy as np
 import os
 
 def test_main():
     fn = os.path.join(os.path.dirname(__file__), 'kintest.csv')
-    df = im.main(fn)
-    assert 0 < df.iloc[0,0] < 1
+    df = importing.main(fn)
+    df.set_index('0', inplace = True)   #reset index as time-series
 
     timepoints = [float(i) for i in list(df)]
     assert len(timepoints) == 2
-#assert test_main.timepoint == 0.004224
+    np.testing.assert_almost_equal(df.iloc[0,0], 0.004224, decimal=5)
 
 def test_three_dplot():
     pass
